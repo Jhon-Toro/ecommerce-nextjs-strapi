eebe2d076077136111920e71665a710efb1c6d38
Jhon-Toro <toromurieljhon@gmail.com>
2025-03-17 23:38:19 -0500

    Added container in banner component

--- frontend/src/app/components/banner/Banner.tsx
+++ frontend/src/app/components/banner/Banner.tsx
@@ -4,9 +4,9 @@ import styles from './Banner.module.scss';
 const Banner = () => {
   return (
     <header className={styles.banner}>
-      <p>Sign up and get 20% off to your first order.{' '} <Link href="/" passHref className={styles.signup}>Sign Up Now</Link></p>
+      <p className={styles.banner__paragraph}>Sign up and get 20% off to your first order.{' '} <Link href="/" passHref className={styles.signup}>Sign Up Now</Link></p>
 
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" aria-label="Close icon" stroke="currentColor" className="size-6">
+      <svg className={styles.banner__close} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" aria-label="Close icon" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
       </svg>
     </header>
