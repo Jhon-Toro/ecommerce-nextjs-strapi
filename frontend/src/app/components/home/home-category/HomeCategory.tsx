0ce8998b47c039bf54720c984ae39e68510f05e2
Jhon-Toro <toromurieljhon@gmail.com>
2025-04-04 20:57:40 -0500

    Finished Home Categories

--- frontend/src/app/components/home/home-category/HomeCategory.tsx
+++ frontend/src/app/components/home/home-category/HomeCategory.tsx
@@ -1,15 +1,30 @@
 import Image from 'next/image';
 import style from './HomeCategory.module.scss';
+import Link from 'next/link';
+import categories from '../../../../assets/home-category.json';
 
 const HomeCategory = () => {
   return (
     <section className={style.category}>
-      <div className={style.category__card}>
-        <h3>Casual</h3>
-        <Image src={''} alt={''}/>
-      </div>
+      {categories.map((category, index) => (
+        <div
+          key={index}
+          className={`${style.category__card} ${category.twoColumn ? style.category__card_twoColumn : ''}`}
+        >
+          <Link href={category.link} className={style.category__card_link}>
+            <h3 className={style.category__card_title}>{category.title}</h3>
+            <Image
+              className={style.category__card_image}
+              src={category.image}
+              alt={category.title}
+              width={407}
+              height={289}
+            />
+          </Link>
+        </div>
+      ))}
     </section>
-  )
+  );
 }
 
-export default HomeCategory
+export default HomeCategory;
