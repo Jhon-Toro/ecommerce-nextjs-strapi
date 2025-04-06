3d7f2664c5ac0287b8f722d13d831e4e70628bc1
Jhon-Toro <toromurieljhon@gmail.com>
2025-04-06 15:09:25 -0500

    Removed product details section

--- frontend/src/app/(pages)/product/[id]/page.tsx
+++ frontend/src/app/(pages)/product/[id]/page.tsx
@@ -1,25 +1,21 @@
 // src/pages/product/[id]/page.tsx
 import React from 'react';
-import ProductInfo from '../../../components/ProductDetails/ProductInfo';
-import ProductImages from '../../../components/ProductDetails/ProductImages';
-import Reviews from '../../../components/ProductDetails/Reviews';
-import mockProduct from '../../../../assets/mockProduct.json';
-import styles from './page.module.scss';
-import { Product } from '../../../components/ProductDetails/productdetails.interface';
+
 
 const ProductPage = () => {
-  const product: Product = mockProduct;
+
 
   return (
-    <div className={styles.container}>
-      <div className={styles.mainSection}>
+    <div >
+      {/* <div className={styles.mainSection}>
         <ProductImages
           mainImage={product.mainImage}
           additionalImages={product.additionalImages}
         />
         <ProductInfo product={product} />
       </div>
-      <Reviews reviews={product.reviews} />
+      <Reviews reviews={product.reviews} /> */}
+      a
     </div>
   );
 };
